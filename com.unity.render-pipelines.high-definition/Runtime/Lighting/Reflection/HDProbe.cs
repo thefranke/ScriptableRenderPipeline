be8ee46d66768f3145579c76cd6ee49e29d1d3fd
JordanL8 <jordanl8@uni.coventry.ac.uk>
2019-09-03 07:31:50 -0600

    Update HDProbe.cs

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/Reflection/HDProbe.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/Reflection/HDProbe.cs
@@ -172,7 +172,7 @@ namespace UnityEngine.Rendering.HighDefinition
         /// <summary>Weight for blending amongst probes (non PBR parameter).</summary>
         public float weight
         { get => m_ProbeSettings.lighting.weight; set => m_ProbeSettings.lighting.weight = value; }
-        /// <summary>Distance at which reflection smoothly fadeout before HDRP cut it completely.</summary>
+        /// <summary>The distance at which reflections smoothly fade out before HDRP cut them completely.</summary>
         public float fadeDistance
         { get => m_ProbeSettings.lighting.fadeDistance; set => m_ProbeSettings.lighting.fadeDistance = value; }
 
