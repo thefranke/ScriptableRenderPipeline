f1082f8ada4847ecb329c4c7925744499e14adce
JordanL8 <lewis.jordan@hotmail.co.uk>
2019-09-19 16:41:50 +0100

    Update VolumeProfileFactory.cs

--- com.unity.render-pipelines.core/Editor/Volume/VolumeProfileFactory.cs
+++ com.unity.render-pipelines.core/Editor/Volume/VolumeProfileFactory.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 namespace UnityEditor.Rendering
 {
     /// <summary>
-    /// An utility class to create volume profile and components.
+    /// A utility class to create Volume Profiles and components.
     /// </summary>
     public static class VolumeProfileFactory
     {
@@ -24,10 +24,10 @@ namespace UnityEditor.Rendering
         }
 
         /// <summary>
-        /// Creates a <see cref="VolumeProfile"/> asset and saves it at the given path.
+        /// Creates a <see cref="VolumeProfile"/> Asset and saves it at the given path.
         /// </summary>
-        /// <param name="path">The path to save the asset to, relative to the project folder</param>
-        /// <returns>The newly created <see cref="VolumeProfile"/></returns>
+        /// <param name="path">The path to save the Asset to, relative to the Project folder.</param>
+        /// <returns>The newly created <see cref="VolumeProfile"/>.</returns>
         public static VolumeProfile CreateVolumeProfileAtPath(string path)
         {
             var profile = ScriptableObject.CreateInstance<VolumeProfile>();
@@ -39,11 +39,11 @@ namespace UnityEditor.Rendering
         }
 
         /// <summary>
-        /// Creates a <see cref="VolumeProfile"/> asset and saves it in a folder next to the scene.
+        /// Creates a <see cref="VolumeProfile"/> Asset and saves it in a folder next to the Scene.
         /// </summary>
-        /// <param name="scene">The scene the profile will be saved next to</param>
-        /// <param name="targetName">A name to use for the asset filename</param>
-        /// <returns>The newly created <see cref="VolumeProfile"/></returns>
+        /// <param name="scene">The Scene to save the Profile next to.</param>
+        /// <param name="targetName">A name to use for the Asset filename.</param>
+        /// <returns>The newly created <see cref="VolumeProfile"/>.</returns>
         public static VolumeProfile CreateVolumeProfile(Scene scene, string targetName)
         {
             string path;
@@ -77,12 +77,12 @@ namespace UnityEditor.Rendering
         /// <summary>
         /// Creates a <see cref="VolumeComponent"/> in an existing <see cref="VolumeProfile"/>.
         /// </summary>
-        /// <typeparam name="T">A type of <see cref="VolumeComponent"/></typeparam>
-        /// <param name="profile">The profile to store the new component in</param>
-        /// <param name="overrides">Should the parameters in the component be overriden?</param>
-        /// <param name="saveAsset">Should the asset be saved? This is useful when you need to
-        /// create several components in a row and only want to save the profile after the latest
-        /// one has been added, as saving assets to disk can be slow.</param>
+        /// <typeparam name="T">A type of <see cref="VolumeComponent"/>.</typeparam>
+        /// <param name="profile">The profile to store the new component in.</param>
+        /// <param name="overrides">specifies whether to override the parameters in the component or not.</param>
+        /// <param name="saveAsset">Specifies whether to save the Profile Asset or not. This is useful when you need to
+        /// create several components in a row and only want to save the Profile Asset after adding the last one,
+        /// because saving Assets to disk can be slow.</param>
         /// <returns></returns>
         public static T CreateVolumeComponent<T>(VolumeProfile profile, bool overrides = false, bool saveAsset = true)
             where T : VolumeComponent
